9003f2c30f383e8a56b308ebded9fc1bc11de5cc
LGCGithub <51138420+LGCGithub@users.noreply.github.com>
2022-12-01 20:23:05 -0300

    Add files via upload

--- classificador1.py
+++ classificador1.py
@@ -13,6 +13,8 @@ from tensorflow.python.keras.optimizers import adam_v2
 from tensorflow.python.keras.metrics import categorical_crossentropy
 from tensorflow.python.keras.models import load_model
 
+from sklearn.metrics import f1_score, precision_score, recall_score, confusion_matrix
+
 import os.path
 
 dataset = pd.read_csv('tar2_sinais_vitais_treino_com_label.txt', encoding='utf-8')
@@ -32,29 +34,70 @@ scaler = MinMaxScaler(feature_range=(0, 1)) # Normalização
 train_samples = scaler.fit_transform(train_samples)
 
 #print(train_samples)
-#print(train_labels)
+#print(train_labels)    
 
-if os.path.isfile("modelos\classificador1.h5") is False:
-    model = Sequential([
-        Dense(units=512, input_shape=(3, ), activation="relu"),
-        Dense(units=256, activation="relu"),
-        Dense(units=4, activation="softmax")
-    ])
+from keras import backend as K
 
-    model.summary()
+def f1_weighted(true, pred): #shapes (batch, 4)
 
-    model.compile(optimizer=adam_v2.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy'])
-    model.fit(x=train_samples, y=train_labels, validation_split=0.3, batch_size=10, epochs=300, shuffle=True, verbose=2)
-
-    model.save("modelos\classificador1.h5")
-else:
-    model = load_model("modelos\classificador1.h5")
-
-predictions = model.predict(x=train_samples, batch_size=10, verbose=0)
-predictions = np.argmax(predictions, axis=-1)
-
-print(predictions)
-print(train_labels_num)
+    #for metrics include these two lines, for loss, don't include them
+    #these are meant to round 'pred' to exactly zeros and ones
+    #predLabels = K.argmax(pred, axis=-1)
+    #pred = K.one_hot(predLabels, 4) 
 
 
-print(np.sum(np.abs(predictions - train_labels_num))) # Absolute error
+    ground_positives = K.sum(true, axis=0) + K.epsilon()       # = TP + FN
+    pred_positives = K.sum(pred, axis=0) + K.epsilon()         # = TP + FP
+    true_positives = K.sum(true * pred, axis=0) + K.epsilon()  # = TP
+        #all with shape (4,)
+    
+    precision = true_positives / pred_positives 
+    recall = true_positives / ground_positives
+        #both = 1 if ground_positives == 0 or pred_positives == 0
+        #shape (4,)
+
+    f1 = 2 * (precision * recall) / (precision + recall + K.epsilon())
+        #still with shape (4,)
+
+    weighted_f1 = f1 * ground_positives / K.sum(ground_positives) 
+    weighted_f1 = K.sum(weighted_f1)
+
+    
+    return weighted_f1 #for metrics, return only 'weighted_f1'
+
+model = Sequential([
+    Dense(units=512, input_shape=(3, ), activation="relu"),
+    Dense(units=256, activation="relu"),
+    Dense(units=4, activation="softmax")
+])
+
+model.summary()
+
+model.compile(optimizer=adam_v2.Adam(learning_rate=0.001), loss='categorical_crossentropy', metrics=['accuracy', f1_weighted])
+model.fit(x=train_samples, y=train_labels, validation_split=0.3, batch_size=10, epochs=300, shuffle=True, verbose=2)
+
+dataset_teste = pd.read_csv('tar2_sinais_vitais_teste_com_label.txt', encoding='utf-8')
+
+dataset_teste = dataset_teste.to_numpy()
+
+test_samples = dataset_teste[:, 3:6] # qPA, pulso, resp
+test_labels = dataset_teste[:, 7] - 1 # classe
+
+test_labels_onehot = onehot_encoder.fit_transform(test_labels.reshape(len(test_labels), 1))
+
+test_labels_num = test_labels
+test_labels = test_labels_onehot
+
+scaler = MinMaxScaler(feature_range=(0, 1)) # Normalização
+test_samples = scaler.fit_transform(test_samples)
+
+predictions = model.predict(x=test_samples, batch_size=1, verbose=2)
+
+from sklearn.metrics import accuracy_score
+
+predictions = np.argmax(predictions, axis=1) 
+test_labels_onehot = np.argmax(test_labels_onehot, axis=1)
+
+print("Accuracy: ", accuracy_score(test_labels_onehot, predictions))
+print("f-measure (weighted):", f1_score(test_labels_onehot, predictions, average="weighted"))
+
--- regressor1.py
+++ regressor1.py
@@ -13,41 +13,50 @@ from tensorflow.python.keras.metrics import categorical_crossentropy
 from tensorflow.python.keras.models import load_model
 
 import os.path
+from keras import backend as K
+
+def root_mean_squared_error(y_true, y_pred):
+    return K.sqrt(K.mean(K.square(y_pred - y_true))) 
+
+#print(train_samples)
+#print(train_labels)
+
+
 
 dataset = pd.read_csv('tar2_sinais_vitais_treino_com_label.txt', encoding='utf-8')
 
 dataset = dataset.to_numpy()
 
 train_samples = dataset[:, 3:6] # qPA, pulso, resp
-train_labels = dataset[:, 6] # classe
+train_labels = dataset[:, 6] # gravidade
 
 scaler = MinMaxScaler(feature_range=(0, 1)) # Normalização
 train_samples = scaler.fit_transform(train_samples)
 
-#print(train_samples)
-#print(train_labels)
+model = Sequential([
+    Dense(units=512, input_shape=(3, ), activation="relu"),
+    Dense(units=256, activation="relu"),
+    Dense(units=1, activation="relu")
+])
 
-if os.path.isfile("modelos\\regressor1.h5") is False:
-    model = Sequential([
-        Dense(units=512, input_shape=(3, ), activation="relu"),
-        Dense(units=256, activation="relu"),
-        Dense(units=1, activation="relu")
-    ])
+model.summary()
 
-    model.summary()
+model.compile(optimizer=adam_v2.Adam(learning_rate=0.001), loss=root_mean_squared_error)
+model.fit(x=train_samples, y=train_labels, validation_split=0.3, batch_size=10, epochs=300, shuffle=True, verbose=2)
 
-    model.compile(optimizer=adam_v2.Adam(learning_rate=0.001), loss='mean_squared_error')
-    model.fit(x=train_samples, y=train_labels, validation_split=0.3, batch_size=10, epochs=300, shuffle=True, verbose=2)
 
-    model.save("modelos\\regressor1.h5")
-else:
-    model = load_model("modelos\\regressor1.h5")
+dataset_teste = pd.read_csv('tar2_sinais_vitais_teste_com_label.txt', encoding='utf-8')
 
-predictions = model.predict(x=train_samples, batch_size=10, verbose=0)
+dataset_teste = dataset_teste.to_numpy()
+
+test_samples = dataset_teste[:, 3:6] # qPA, pulso, resp
+test_labels = dataset_teste[:, 6] # gravidade
+
+scaler = MinMaxScaler(feature_range=(0, 1)) # Normalização
+test_samples = scaler.fit_transform(test_samples)
+
+predictions = model.predict(x=test_samples, batch_size=1, verbose=2)
 
 predictions = predictions.reshape(1, -1)
 
-print(predictions)
-print(train_labels)
-
-print(np.sum(np.abs(predictions - train_labels))) # Absolute error
+print("RMSE: ", np.sqrt(np.mean(np.square(predictions - test_labels))))
